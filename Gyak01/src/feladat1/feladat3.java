3fb0b99aa741507ea61c49966423b9d02d86325f
NagyBoldi99 <boboldi1999@gmail.com>
2022-04-19 12:52:54 +0200

    gyak1

---
+++ Gyak01/src/feladat1/feladat3.java
@@ -0,0 +1,12 @@
+package feladat1;
+
+public class feladat3 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int)(Math.random() * 100);
+		}
+	}
+
+}
